5fcc8762ed2880b8aae4a25dcfd714936a45d60a
dabudidabuday <94mir.ms@gmail.com>
2019-09-12 18:33:27 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+let {gulp, src, dest, watch, series, parallel} = require('gulp');
+let sass = require('gulp-sass');
+let concat = require('gulp-concat');
+
+function compile() {
+	return src('app/scss/index.scss') 
+	.pipe(sass())
+	.pipe(concat('index.css'))
+	.pipe(dest('dist'))
+}
+
+exports.compile = compile;
+
+function watcher() {
+	watch('app/scss/index.scss').on('change', compile);
+}
+
+exports.watcher = watcher;
+
